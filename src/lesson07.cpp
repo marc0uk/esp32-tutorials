b4afac27ecac93e0f8060bac0c74e1275cc7d6fd
Marco Riello <mriello@gmail.com>
2026-04-25 21:36:16 +0100

    Successfully executed lesson 7

---
+++ src/lesson07.cpp
@@ -0,0 +1,32 @@
+#include<Arduino.h>
+#include "lesson07.h"
+
+// Create An LCD Object. Signals: [ RS, EN, D4, D5, D6, D7 ] 
+LiquidCrystal My_LCD(13, 12, 14, 27, 26, 25);
+
+void setup_lesson07() {
+    Serial.begin(115200);
+    delay(200);
+    Serial.println("Initialising LCD - lesson07");
+
+    // Initialise the LCD. Parameter: [ columns, rows ]
+    My_LCD.begin(16, 2);
+    // Clears the LCD display
+    My_LCD.clear();
+
+    // Display the first message in home position (0, 0)
+    Serial.println("Hello world");
+    My_LCD.print("Hello world");
+    // Set the cursor position to [ col 5, row 1]
+    // The next message will start from the 6th char position in the 2nd row
+    // Note: 1st row has an index of 0
+    My_LCD.setCursor(5, 1);
+    // Display the second message in position (5, 1)
+    Serial.println("GG izi");
+    My_LCD.print("GG izi");
+    Serial.println("END!");
+}
+
+void loop_lesson07() {
+
+}
---
+++ src/lesson07.h
@@ -0,0 +1,8 @@
+#ifndef LESSON07_H
+#define LESSON07_H
+#include<LiquidCrystal.h>
+
+void setup_lesson07();
+void loop_lesson07();
+
+#endif
